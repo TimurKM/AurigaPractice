fea19979e9769b9bdf40acc23be30a4d73717896
Timur <kakabaevtm@mail.ru>
2021-11-24 22:42:39 +0300

    Trial of try/catch handling

--- TaskWithNullptr/TaskWithNullptr.cpp
+++ TaskWithNullptr/TaskWithNullptr.cpp
@@ -1,9 +1,16 @@
 ﻿#include <iostream>
 
-
 int main()
 {
-	double* ptr = nullptr; // create a null pointer 
-	std::cout << *ptr; // dereference a null pointer, exception will trigger
+	try
+	{
+		double* ptr = nullptr; // create a null pointer 
+		std::cout << *ptr; // dereference a null pointer, exception will trigger
+	}
+	catch (const std::exception&) // catch does not work because dereferncing null pointer is undefined behaviour
+	{
+		std::cout << "It is nullptr" << std::endl;
+	}
+
 	return 0;
 }
