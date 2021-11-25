bedb16e102eabe24b6aba6fff3286e7332827b59
Timur <kakabaevtm@mail.ru>
2021-11-25 15:29:58 +0300

    Using cast in task

---
+++ TaskWithUsingCast/TaskWithUsingCast.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main()
+{
+	char a = 0;
+	char b = '0';
+	std::cout << "a: " << static_cast<int>(a) << std::endl;
+	std::cout << b << " has ASCII code " << static_cast<int>(b) << std::endl;
+}
