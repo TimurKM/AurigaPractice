a10bd32414d4657cb89d592fb0900bd260414750
Timur <kakabaevtm@mail.ru>
2021-11-25 15:00:35 +0300

    Using of strlen function

---
+++ TaskWithStrlen/TaskWithStrlen.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int strLength(char str[]) {
+	int count = 0;
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+
+	return count;
+}
+
+int main()
+{
+	char str[] = "Hello World";
+
+	std::cout << strLength(str) << std::endl;
+
+	std::cout << strlen(str) << std::endl;
+
+	for (int i = 0; i < sizeof(str); i++)
+	{
+		if (str[i] == '\0')
+		{
+			str[i] = 'd';
+		}
+	}
+
+	std::cout << strLength(str) << std::endl;
+
+	std::cout << strlen(str) << std::endl;
+
+	return 0;
+}
