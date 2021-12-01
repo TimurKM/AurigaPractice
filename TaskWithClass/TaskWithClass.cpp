77d23e54e6ce71d3f30a5f3e9325bee2e6cb5d45
Timur <kakabaevtm@mail.ru>
2021-12-01 13:10:54 +0300

    Beginning of work with classes

---
+++ TaskWithClass/TaskWithClass.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+class A {
+
+};
+
+class String {
+public:
+	std::string c_str() {
+		return std::string();
+	}
+	int length() {
+		return 0;
+	}
+	int size() {
+		return 0;
+	}
+	String() {
+		std::cout << "Constructor: " << std::endl;
+	}
+	~String() {
+		std::cout << "Destructor: " << std::endl;
+	}
+private:
+	char* m_str;
+	size_t m_len;
+};
+int main()
+{
+	std::cout << sizeof(A) << std::endl;
+	String str;
+	return 0;
+}
+
