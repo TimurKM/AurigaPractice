b085e22f7f7063f5cb59f5c3bc5455953f48d3a5
Timur <kakabaevtm@mail.ru>
2021-12-15 22:12:46 +0300

    First version of server

---
+++ TCPEchoServer/Client.cpp
@@ -0,0 +1,84 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+
+	WSADATA wsaData;
+	int iResult;
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	const int default_buflen = 1024;
+	int recvbuflen = default_buflen;
+	const char* default_port = "8080";
+	std::string sendbuf;
+	std::cout << "Input your name: ";
+	std::getline(std::cin, sendbuf);
+	const char* c = sendbuf.c_str();
+	char recvbuf[default_buflen];
+	ZeroMemory(recvbuf, sizeof(recvbuf));
+	struct addrinfo* result = NULL, * ptr = NULL, hints;
+
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0)
+	{
+		std::cout << "WSAStartup failed: " << iResult << std::endl;
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	iResult = getaddrinfo(argv[1], default_port, &hints, &result);
+	if (iResult != 0)
+	{
+		std::cout << "Getaddrinfo failed: " << iResult << std::endl;
+	}
+
+	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+		if (ConnectSocket == INVALID_SOCKET)
+		{
+			std::cout << "Socket failed with error: " << WSAGetLastError() << std::endl;
+		}
+
+		iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+		if (iResult == SOCKET_ERROR) {
+			closesocket(ConnectSocket);
+			ConnectSocket = INVALID_SOCKET;
+			continue;
+		}
+		break;
+	}
+
+	freeaddrinfo(result);
+
+	if (ConnectSocket == INVALID_SOCKET)
+	{
+		std::cout << "Unable to connect to server!" << std::endl;
+	}
+
+	iResult = send(ConnectSocket, c, (int)strlen(c) + 1, 0);
+	if (iResult == SOCKET_ERROR)
+	{
+		std::cout << "Send failed: " << WSAGetLastError() << std::endl;
+	}
+
+	std::cout << "Bytes Sent: " << iResult << std::endl;
+
+	iResult = shutdown(ConnectSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR)
+	{
+		std::cout << "Shutdown failed: " << WSAGetLastError() << std::endl;
+	}
+
+	closesocket(ConnectSocket);
+	WSACleanup();
+	return 0;
+}
---
+++ TCPEchoServer/Server.cpp
@@ -0,0 +1,106 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+	WSADATA wsaData;
+	int iResult;
+	SOCKET ListenSocket = INVALID_SOCKET;
+	SOCKET ClientSocket = INVALID_SOCKET;
+	struct addrinfo* result = NULL;
+	struct addrinfo hints;
+	int iSendResult;
+	const int default_buflen = 1024;
+	const char* default_port = "8080";
+	fd_set rset;
+
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0)
+	{
+		std::cout << "WSAStartup failed: " << iResult << std::endl;
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	iResult = getaddrinfo(NULL, default_port, &hints, &result);
+	if (iResult != 0)
+	{
+		std::cout << "Getaddrinfo failed: " << iResult << std::endl;
+	}
+
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET)
+	{
+		std::cout << "Error at socket(): " << WSAGetLastError() << std::endl;
+	}
+
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR)
+	{
+		std::cout << "Bind failed with error: " << WSAGetLastError() << std::endl;
+	}
+
+	freeaddrinfo(result);
+
+	iResult = listen(ListenSocket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR)
+	{
+		std::cout << "Listen failed with error: " << WSAGetLastError() << std::endl;
+	}
+
+	FD_ZERO(&rset);
+	char recvbuf[default_buflen];
+	int recvbuflen = default_buflen;
+	int maxfdp1 = ListenSocket + 1;
+	for (;;)
+	{
+		FD_SET(ListenSocket, &rset);
+		int nready = select(maxfdp1, &rset, NULL, NULL, NULL);
+		if (FD_ISSET(ListenSocket, &rset))
+		{
+			ClientSocket = accept(ListenSocket, NULL, NULL);
+			if (ClientSocket == INVALID_SOCKET)
+			{
+				std::cout << "Accept failed: " << WSAGetLastError() << std::endl;
+				break;
+			}
+
+			ZeroMemory(recvbuf, sizeof(recvbuf));
+			iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+			if (iResult > 0)
+			{
+				std::cout << "Bytes received: " << iResult << std::endl;
+				puts(recvbuf);
+			}
+			else if (iResult == 0)
+			{
+				std::cout << "Connection closing..." << std::endl;
+			}
+			else
+			{
+				std::cout << "Recv failed: " << WSAGetLastError() << std::endl;
+			}
+		}
+	}
+
+	iResult = shutdown(ListenSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR)
+	{
+		std::cout << "Shutdown failed: %d\n" << WSAGetLastError() << std::endl;
+	}
+
+	closesocket(ListenSocket);
+	WSACleanup();
+	return 0;
+};
