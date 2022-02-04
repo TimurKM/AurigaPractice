9ff810bb00d1bde906b22cc24dc74c2ac642020d
Timur <kakabaevtm@mail.ru>
2022-02-04 19:08:02 +0300

    Program which get a computer name from register

---
+++ TaskWithGettingNameOfComputer/TaskWithGettingNameOfComputer.cpp
@@ -0,0 +1,23 @@
+﻿#include <windows.h>
+#include <winreg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main()
+{
+	unsigned char pszName[256];
+	DWORD nNameLen = 256;
+	HKEY hkResult, hStartKey = HKEY_LOCAL_MACHINE;
+	long nResult = ERROR_SUCCESS;
+	nResult = RegOpenKeyEx(hStartKey, L"SYSTEM\\CurrentControlSet\\Control\\ComputerName\\ActiveComputerName", 0L, KEY_READ, &hkResult);
+	if (ERROR_SUCCESS == nResult)
+	{
+		nResult = RegQueryValueExA(hkResult, "ComputerName", 0, 0, pszName, &nNameLen);
+		if (ERROR_SUCCESS == nResult)
+		{
+			std::cout << pszName << std::endl;
+		}
+	}
+	RegCloseKey(hkResult);
+}
