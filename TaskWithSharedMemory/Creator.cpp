751c007199ab2aea24453738ddf461008504221c
Timur <kakabaevtm@mail.ru>
2022-02-09 22:58:33 +0300

    Work with shared memory

---
+++ TaskWithSharedMemory/Creator.cpp
@@ -0,0 +1,50 @@
+﻿#include <windows.h>
+#include <conio.h>
+#include <iostream>
+#include <functional>
+
+TCHAR szName[] = TEXT("Global\\MyFileMappingObject");
+
+int main()
+{
+	HANDLE hMapFile;
+	DWORD* pBuf;
+
+	hMapFile = CreateFileMapping(
+		INVALID_HANDLE_VALUE,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		sizeof(DWORD),
+		szName);
+
+	if (hMapFile == NULL)
+	{
+		std::wcout << "Could not create file mapping object: " << GetLastError() << std::endl;
+		return 1;
+	}
+
+	SetEvent(hMapFile);
+
+	pBuf = (DWORD*)MapViewOfFile(hMapFile,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		0,
+		sizeof(DWORD));
+
+	if (pBuf == NULL)
+	{
+		std::wcout << "Could not map view of file: " << GetLastError() << std::endl;
+		return 1;
+	}
+
+	DWORD pid = GetCurrentProcessId();
+	std::cout << pid << std::endl;
+	*pBuf = pid;
+	_getch();
+
+	UnmapViewOfFile(pBuf);
+
+	std::unique_ptr<void, std::function<void(HANDLE)>> wrap(hMapFile, [](HANDLE h) { ::CloseHandle(h); });
+	return 0;
+}
---
+++ TaskWithSharedMemory/Opener.cpp
@@ -0,0 +1,50 @@
+﻿#include <windows.h>
+#include <conio.h>
+#include <iostream>
+#include <functional>
+
+TCHAR szName[] = TEXT("Global\\MyFileMappingObject");
+
+int main()
+{
+	HANDLE hMapFile;
+	DWORD* pBuf;
+
+	hMapFile = OpenFileMapping(
+		FILE_MAP_ALL_ACCESS,
+		FALSE,
+		szName);
+
+	if (hMapFile == NULL)
+	{
+		std::wcout << "Could not open file mapping object: " << GetLastError() << std::endl;
+		return 1;
+	}
+
+	DWORD dwWaitResult;
+
+	dwWaitResult = WaitForSingleObject(
+		hMapFile,
+		INFINITE);
+
+	pBuf = (DWORD*)MapViewOfFile(hMapFile,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		0,
+		sizeof(DWORD));
+
+	if (pBuf == NULL)
+	{
+		std::wcout << "Could not map view of file: " << GetLastError() << std::endl;
+
+		return 1;
+	}
+
+	DWORD pid = *reinterpret_cast<DWORD*>(pBuf);
+	std::cout << pid << std::endl;
+	_getch();
+	UnmapViewOfFile(pBuf);
+
+	std::unique_ptr<void, std::function<void(HANDLE)>> wrap(hMapFile, [](HANDLE h) { ::CloseHandle(h); });
+	return 0;
+}
