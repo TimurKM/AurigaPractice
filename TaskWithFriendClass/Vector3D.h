66daecc4884f24da528cdfca0a4b408bbbeea6fa
Timur <kakabaevtm@mail.ru>
2022-03-23 12:34:47 +0300

    Task with using of friend class

---
+++ TaskWithFriendClass/Vector3D.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Point3D.h"
+
+class Vector3D
+{
+private:
+	double m_x, m_y, m_z;
+
+public:
+	Vector3D(double x = 0.0, double y = 0.0, double z = 0.0);
+
+	void print();
+
+	friend void Point3D::moveByVector(const Vector3D& v);
+};
---
+++ TaskWithFriendClass/main.cpp
@@ -0,0 +1,15 @@
+﻿#include "Point3D.h"
+#include "Vector3D.h"
+#include <iostream>
+
+int main()
+{
+	Point3D p(3.0, 4.0, 5.0);
+	Vector3D v(3.0, 3.0, -2.0);
+
+	p.print();
+	p.moveByVector(v);
+	p.print();
+
+	return 0;
+}
